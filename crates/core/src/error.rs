f246187a3481ad5df6c963de8c0d87a7dee53207
Bhargav Kantheti <k@bk.bio>
2026-03-18 12:35:39 -0500

    ADD impls for error

--- crates/core/src/error.rs
+++ crates/core/src/error.rs
@@ -9,3 +9,39 @@ pub enum Error {
     Knit(String),
     Unknit(String),
 }
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Io(e) => write!(f, "io error: {e}"),
+            Self::Parse(e) => write!(f, "parse error: {e}"),
+            Self::Suture(msg) => write!(f, "suture error: {msg}"),
+            Self::Stitch(msg) => write!(f, "stitch error: {msg}"),
+            Self::Unstitch(msg) => write!(f, "unstitch error: {msg}"),
+            Self::Knit(msg) => write!(f, "knit error: {msg}"),
+            Self::Unknit(msg) => write!(f, "unknit error: {msg}"),
+        }
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Self::Io(e) => Some(e),
+            Self::Parse(e) => Some(e),
+            _ => None,
+        }
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(e: std::io::Error) -> Self {
+        Self::Io(e)
+    }
+}
+
+impl From<serde_json::Error> for Error {
+    fn from(e: serde_json::Error) -> Self {
+        Self::Parse(e)
+    }
+}
