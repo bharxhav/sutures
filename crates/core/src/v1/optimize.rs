b01cf4c1c4a10dc77c106351540c34720cab2820
Bhargav Kantheti <k@bk.bio>
2026-03-21 09:22:05 -0500

    RM opt

--- crates/core/src/v1/optimize.rs
+++
@@ -1,8 +0,0 @@
-use super::schema::RawSutureSet;
-use super::suture::Suture;
-use crate::error::Error;
-
-/// Compile a single suture_set.
-pub(super) fn compile_suture_set(set: RawSutureSet) -> Result<Suture, Error> {
-    todo!("compile: compile_suture_set")
-}
